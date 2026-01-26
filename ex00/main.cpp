15fce19d7d9a1eae9db572686c7705b8a55053c6
yusuuf-dev <lucas2030garcia@gmail.com>
2026-01-26 16:47:59 +0100

    add more tests to main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,17 +1,32 @@
 #include "easyfind.hpp"
 #include <vector>
+#include <list>
 
+template <typename T> void print(const T& container)
+{
+    std::cout << "\nprinting container sequences\n";
+    for (typename T::const_iterator it = container.begin();it != container.end();it++)
+    {
+        std:: cout << *it << '\t';
+    }
+    std::cout << std::endl;
+}
 
 int main()
 {
-    std::vector<int> v1(10);
+    std::vector<int> v(5,1);
+    print(v);
+    easyfind(v,11);
+    v.push_back(11);
+    print<std::vector<int> >(v);
+    easyfind(v,11);
 
-    for (size_t i = 0; i < v1.size(); i++)
-        v1[i] = i + 1;
-    
-    easyfind(v1,11);
-    v1.push_back(11);
-    easyfind(v1,11);
+    std::list<int> l(5,4);
+    print(l);
+    easyfind(v,12);
+    l.push_front(12);
+    print(l);
+    easyfind(l,12);
 
     return 0;
 }
